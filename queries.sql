7b055d3fb3e871df4254c534239ba6ebcc608c46
Mitchell Wright <mitchellbwright@gmail.com>
2020-10-20 21:49:21 -0500

    Finish third query

--- queries.sql
+++ queries.sql
@@ -7,7 +7,18 @@ JOIN Category AS C
 ON C.Id = P.CategoryId;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT O.Id, S.CompanyName
+FROM 'Order' AS O
+JOIN Shipper as S
+ON S.Id = O.ShipVia
+WHERE O.OrderDate < '2012-08-09'
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+SELECT P.ProductName, O.Quantity
+FROM OrderDetail AS O
+JOIN Product AS P
+ON P.Id = O.ProductId
+WHERE OrderId = 10251
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
